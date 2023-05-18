420142f7297f126cfc7857ac36ceec4e9eb04307
roman_harazha <roman.harazha@gmail.com>
2023-05-18 15:41:52 +0300

    remove block range

--- internal/service/indexer.go
+++ internal/service/indexer.go
@@ -51,7 +51,6 @@ func newIndexer(c config.Config, lastBlock uint64) indexer {
 		ethClient:       c.Network().EthClient,
 		wsClient:        c.Network().WsClient,
 		chainID:         c.Network().ChainID,
-		blockRange:      c.Network().BlockRange,
 		lastBlock:       lastBlock,
 		requestTimeout:  c.Network().RequestTimeout,
 		swapicaAbi:      swapicaAbi,
